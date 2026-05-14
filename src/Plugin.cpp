c107443a363fda0ad972fe0fcd603dc70b1af0bb
codepuncher <lee.hanbury@gmail.com>
2026-05-14 19:06:33 +0100

    feat: initial template

---
+++ src/Plugin.cpp
@@ -0,0 +1,54 @@
+#include "PCH.h"
+
+namespace logger = SKSE::log;
+
+void SetupLog()
+{
+	auto logsFolder = logger::log_directory();
+	if (!logsFolder) {
+		SKSE::stl::report_and_fail("SKSE log_directory not provided, logs can't be written");
+	}
+
+	auto logPath = *logsFolder / "ExampleMod.log";
+	auto fileSink = std::make_shared<spdlog::sinks::basic_file_sink_mt>(logPath.string(), true);
+	auto spdlogger = std::make_shared<spdlog::logger>("global", std::move(fileSink));
+	spdlog::set_default_logger(std::move(spdlogger));
+	spdlog::set_level(spdlog::level::info);
+	spdlog::flush_on(spdlog::level::info);
+}
+
+void OnDataLoaded()
+{
+	logger::info("OnDataLoaded hook fired");
+
+	if (auto* const console = RE::ConsoleLog::GetSingleton()) {
+		console->Print("[ExampleMod] Loaded successfully!");
+	}
+}
+
+SKSEPluginLoad(const SKSE::LoadInterface* a_skse)
+{
+	SKSE::Init(a_skse);
+	SetupLog();
+
+	const auto* plugin = SKSE::PluginDeclaration::GetSingleton();
+	logger::info("{} v{} loaded", plugin->GetName(), plugin->GetVersion());
+
+	const auto* messaging = SKSE::GetMessagingInterface();
+	if (messaging == nullptr) {
+		logger::error("Failed to get SKSE messaging interface");
+		return false;
+	}
+
+	messaging->RegisterListener([](SKSE::MessagingInterface::Message* a_msg) {
+		switch (a_msg->type) {
+		case SKSE::MessagingInterface::kDataLoaded:
+			OnDataLoaded();
+			break;
+		default:
+			break;
+		}
+	});
+
+	return true;
+}
